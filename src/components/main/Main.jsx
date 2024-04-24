5ce975e45c5aec4e302b5c740db217e18befd9ff
Titan <68808479+titan3755@users.noreply.github.com>
2024-04-24 15:13:18 +0600

    Modal added

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,29 +1,90 @@
+import { useState } from "react"
 import FormComp from "./main_comps/Form"
 import FunctionBarComp from "./main_comps/FunctionBar"
 import ItemListComp from "./main_comps/ItemList"
 import TitleComp from "./main_comps/Title"
 
 export default function Main() {
+    const [modalState, setModalState] = useState(false)
+    const [taskTitle, setTaskTitle] = useState('')
     return (
         <>
             <div className="w-full min-h-screen bg-white shadow-inner p-24 flex justify-center align-middle flex-col">
                 <div className="bg-white shadow-2xl border-gray-300 border rounded-xl w-full min-h-max p-10">
-                    <MainBox />
+                    <MainBox taskTitle={taskTitle} setTaskTitle={setTaskTitle} modalState={modalState} setModalState={setModalState} />
                 </div>
+            </div>
+            {modalState ? <ModalBox taskTitle={taskTitle} setTaskTitle={setTaskTitle} modalState={modalState} setModalState={setModalState} /> : <></>}
+        </>
+    )
+}
+
+function MainBox(props) {
+    return (
+        <>
+            <div className="flex flex-col gap-12 justify-center px-12">
+                <TitleComp />
+                <FormComp taskTitle={props.taskTitle} setTaskTitle={props.setTaskTitle} modalState={props.modalState} setModalState={props.setModalState} />
+                <FunctionBarComp />
+                <ItemListComp />
             </div>
         </>
     )
 }
 
-function MainBox() {
+function ModalBox(props) {
+    const [taskDesc, setTaskDesc] = useState('')
     return (
         <>
-            <div className="flex flex-col gap-12 justify-center px-12">
-                <TitleComp />
-                <FormComp />
-                <FunctionBarComp />
-                <ItemListComp />
+            <form>
+            <div className="fixed top-1/2 left-1/2 -translate-x-1/2 backdrop-blur-3xl overflow-y-scroll -translate-y-1/2 justify-center items-center w-full h-full">
+                <div className="relative p-10 w-full">
+                    <div className="relative bg-white rounded-lg shadow">
+                        <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t">
+                            <h3 className="text-xl font-semibold text-gray-900">
+                                Additional Task Information
+                            </h3>
+                        </div>
+                        <div className="p-4 md:p-5 space-y-4">
+                            <p className="text-base leading-relaxed text-gray-500">
+                                Some additional information is required regarding your task. Type the information onto the input fields below, you can leave some fields empty.
+                            </p>
+                            <div class="mb-6">
+                                <label htmlFor="default-input" className="block mb-2 text-lg font-medium text-gray-900">Task Title</label>
+                                <input type="text" id="default-input" value={props.taskTitle} onChange={(e) => {props.setTaskTitle(e.target.value)}} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
+                            </div>
+                            <div class="mb-6">
+                                <label htmlFor="default-input" className="block mb-2 text-lg font-medium text-gray-900">Task Description</label>
+                                <textarea rows={20} id="default-input" value={taskDesc} onChange={(e) => {setTaskDesc(e.target.value)}} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
+                            </div>
+                            <div className="mb-6">
+                                <label htmlFor="time" className="block mb-2 text-lg font-medium text-gray-900">Select deadline time</label>
+                                <div className="relative">
+                                    <div className="absolute inset-y-0 end-0 top-0 flex items-center pe-3.5 pointer-events-none">
+                                        <svg className="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                                            <path fillRule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4a1 1 0 1 0-2 0v4a1 1 0 0 0 .293.707l3 3a1 1 0 0 0 1.414-1.414L13 11.586V8Z" clipRule="evenodd"/>
+                                        </svg>
+                                    </div>
+                                    <input type="time" id="time" className="bg-gray-50 border leading-none border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" min="09:00" max="18:00" value="00:00" required />
+                                </div>
+                            </div>
+                            <div className="mb-6">
+                                <label htmlFor="date" className="block mb-2 text-lg font-medium text-gray-900">Select deadline date</label>
+                                <div className="relative">
+                                    <input type="date" id="date" className="bg-gray-50 border leading-none border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
+                                </div>
+                            </div>
+                            <div className="mb-6">
+                                
+                            </div>
+                        </div>
+                        <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b">
+                            <button type="button" onClick={() => {props.setModalState(false)}} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Add Task</button>
+                        </div>
+                    </div>
+                </div>
             </div>
+            </form>
         </>
     )
 }
--- src/components/main/main_comps/FunctionBar.jsx
+++ src/components/main/main_comps/FunctionBar.jsx
@@ -29,7 +29,7 @@ export default function FunctionBarComp() {
                 </div>
                 </div>
                 <div className="">
-                    <select className="py-4 px-4 pe-20 block w-full bg-gray-100 border-transparent rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none">
+                    <select id="sort-menu" className="py-4 px-4 pe-20 block w-full bg-gray-100 border-transparent rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none">
                         <option value="">Sort By</option>
                         <option>Date Added</option>
                         <option>Name</option>
